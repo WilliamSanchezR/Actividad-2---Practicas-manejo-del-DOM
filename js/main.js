7be369e9a119437745c4f38b7da78577e93c45dc
William Sanchez <183452295+WilliamSanchezR@users.noreply.github.com~>
2025-09-04 16:04:21 -0500

    Agregue la manipulacion de atributos con js y unas pequeñas correcciones de las primeras manipulaciones

--- js/main.js
+++ js/main.js
@@ -17,7 +17,16 @@ const figura = document.getElementById('figure');
     }
 
     function animacion () {
-        figura.style.transform = "rotate(360deg)";
+        let style = document.createElement("style");
+
+        style.innerHTML = `
+        @keyframes girar {
+            from { transform: rotate(0deg); }
+            to { transform: rotate(360deg); }
+        }
+        `;
+        document.head.appendChild(style);
+        figura.style.animation = "girar 2s linear infinite";
     }
 
     function moveTop () {
@@ -61,6 +70,77 @@ const figura = document.getElementById('figure');
             deleteParrafo.remove();
     }
 
+// contenidos
+
+    function hex () {
+        figura.style.backgroundColor = prompt("Escribe un valor en formato HEX, similar al siguiente: #000000")
+    }
+
+    function elegirFigura () {
+        let valor = prompt("Elige que figura deseas: \n 1. Circulo \n 2. Estrella \n 3. Cuadrado");
+        
+        switch (valor) {
+            case "1": circulo();
+                break;
+            case "2": star();
+                    figura.style.backgroundColor = "black";
+                break;
+            case "3": figura.style.width = "300px";
+                    figura.style.height = "300px";
+                    figura.style.backgroundColor = "blue";
+                break;
+            default: alert("Opcion no valida")
+                break;
+        }
+    }
+
+    const arregloImagenes = ["img/imagen.jpg","img/imagen2.jpg","img/imagen3.jpg"];
+    let index = 0;
+    const imagen = document.getElementById("mainImage");
+
+    function imagenN () {
+        index = (index + 1) % arregloImagenes.length;
+        imagen.src = arregloImagenes[index];
+    }
+
+    function imagenB () {
+        index = (index - 1);
+        imagen.src = arregloImagenes[index];
+    }
+
+    function mostrarOcultar () {
+        if (imagen.style.display === "none") {
+            imagen.style.display = "block";
+            imagen.style.margin = "20px auto";
+            } else {
+                imagen.style.display = "none";
+            }
+    }
+
+    function changeAlt () {
+        let Alt = prompt("Agrega el nuevo texto alternativo de la imagen");
+            imagen.setAttribute('alt', Alt);
+    }
+
+    const etiquetaA = document.getElementById('mainLink');
+
+    function changeUrl () {
+        let nuevaUrl = prompt("Cambia la url por la que tengas en mente");
+            etiquetaA.setAttribute("href", nuevaUrl);
+    }
+
+    function addAtributo () {
+        etiquetaA.setAttribute("target", "_blank");
+    }
+
+    function remove(){
+
+    etiquetaA.removeAttribute("href");
+    etiquetaA.style.color = "gray";
+    etiquetaA.style.textDecoration = "none";
+
+}
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -76,4 +156,13 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("btnChangeParagraph").addEventListener("click", parrafo);
     document.getElementById("btnAddParagraph").addEventListener("click", addParrafo);
     document.getElementById("btnRemoveParagraph").addEventListener("click", deleteParrafo);
+    document.getElementById("btnHexColor").addEventListener("click", hex);
+    document.getElementById("btnChooseFigure").addEventListener("click", elegirFigura);
+    document.getElementById("btnChangeImageNext").addEventListener("click", imagenN);
+    document.getElementById("btnChangeImagePrev").addEventListener("click", imagenB);
+    document.getElementById("btnToggleImage").addEventListener("click", mostrarOcultar);
+    document.getElementById("btnChangeAlt").addEventListener("click", changeAlt);
+    document.getElementById("btnChangeLink").addEventListener("click", changeUrl);
+    document.getElementById("btnOpenLink").addEventListener("click", addAtributo);
+    document.getElementById("btnDisableLink").addEventListener("click", remove);
 });
